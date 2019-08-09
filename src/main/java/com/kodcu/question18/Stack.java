64fca926f3c1202c4ed001cddfa9fbdf1fceda0a
AltugAltintas <altuga@gmail.com>
2019-08-09 13:50:48 +0300

    quesstion 18  done

---
+++ src/main/java/com/kodcu/question18/Stack.java
@@ -0,0 +1,40 @@
+package com.kodcu.question18;
+
+
+import java.util.Arrays;
+
+/*
+ 1 - Add String value to the Stack
+ 2 - Try to convert String value to uppercase
+ 3 - What is the lesson  ?
+*/
+
+public class Stack {
+    private Object[] elements;
+    private int size = 0;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    public Stack() {
+        elements = new Object[DEFAULT_INITIAL_CAPACITY];
+    }
+    public void push(Object e) {
+        ensureCapacity();
+        elements[size++] = e;
+    }
+    public Object pop() {
+        if (size == 0)
+            throw new EmptyStackException();
+        Object result = elements[--size];
+        elements[size] = null; // Eliminate obsolete reference
+        return result;
+    }
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    private void ensureCapacity() {
+        if (elements.length == size)
+            elements = Arrays.copyOf(elements, 2 * size + 1);
+    }
+
+
+}
