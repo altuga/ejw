7fc3023642e0c9f81625e6fb66bbe681b1f268f5
AltugAltintas <altuga@gmail.com>
2019-08-13 09:51:49 +0300

    testing question 21

--- src/main/java/com/kodcu/question21/Main.java
+++ src/main/java/com/kodcu/question21/Main.java
@@ -3,19 +3,23 @@ package com.kodcu.question21;
 
 /**
  * TODO
- * 1 - How to calculate WeightTable in a for-loop - 5 minutes
+ * 1 - How to calculate your  weight on all eight planets  - 5 minutes
  * 2 - Instructor will show the solutions
  * 3 - What is the lesson  ?
  */
 public class Main {
 
     public static void main(String[] args) {
+
+        /**
+         * If your Weight is 80 on Earth then what will be on Mars? Or Uranus ? ...
+         */
         double earthWeight = Double.parseDouble(args[0]);
 
         double result = Calculator.
-                calculateSurfaceWeight(Calculator.MARS_REDIUS,
-                        Calculator.MARS_MASS, earthWeight);
-        System.out.printf("Weight on %s is %f%n", "Calculator.MARS_REDIUS", result);
+                calculateSurfaceWeight(Calculator.JUPITER_REDIUS,
+                        Calculator.JUPITER_MASS, earthWeight);
+        System.out.println("Weight on EARTH = " + result);
 
         /**
          * for (Planets p  ...) {
