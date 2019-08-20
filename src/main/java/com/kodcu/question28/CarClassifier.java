cae585cdc70c62ee8825745747cea8233a8e2d99
AltugAltintas <altuga@gmail.com>
2019-08-20 22:55:47 +0300

    question 28 is done

--- src/main/java/com/kodcu/question28/CarClassifier.java
+++ src/main/java/com/kodcu/question28/CarClassifier.java
@@ -1,33 +1,42 @@
 package com.kodcu.question28;
 
 
+import java.util.List;
+
 /**
+ *
+ * TODO
+ *
  * 1 - Run CarClassifier.java
  * 2 - What does this program print?
- * 3 - Could you please fix it ?
+ * 3 - How can you print "Car - BMW - Mercedes"  ?
  */
 public class CarClassifier {
 
-    public static String classify(Car car) {
+    public  String classify(Car car) {
         return "Car";
     }
 
-    public static String classify(BMW bmw) {
+
+    public  String classify(BMW bmw) {
         return "BMW";
     }
 
-    public static String classify(Mercedes mercedes) {
+
+    public  String classify(Mercedes mercedes) {
         return "Mercedes";
     }
 
     public static void main(String[] args) {
-        Car[] collections = {
+        List<Car> collections = List.of(
                 new Car(),
                 new Mercedes(),
                 new BMW()
-        };
+        );
+
+        CarClassifier carClassifier = new CarClassifier();
 
         for (Car car : collections)
-            System.out.println(classify(car));
+            System.out.println(carClassifier.classify(car));
     }
 }
