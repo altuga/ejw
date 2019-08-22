d075fc27e9953d5a008725bf6a9bbd7ad6f60819
AltugAltintas <altuga@gmail.com>
2019-08-22 15:53:03 +0300

    question 31 is done

--- src/main/java/com/kodcu/question31/Max.java
+++ src/main/java/com/kodcu/question31/Max.java
@@ -24,33 +24,13 @@ public class Max {
         return result;
     }
 
-//    // Returns maximum value in collection as an Optional<E> (Page 250)
-//    public static <E extends Comparable<E>>
-//    Optional<E> max(Collection<E> c) {
-//        if (c.isEmpty())
-//            return Optional.empty();
-//
-//        E result = null;
-//        for (E e : c)
-//            if (result == null || e.compareTo(result) > 0)
-//                result = Objects.requireNonNull(e);
-//
-//        return Optional.of(result);
-//    }
 
-    // Returns max val in collection as Optional<E> - uses stream (Page 250)
-//    public static <E extends Comparable<E>>
-//    Optional<E> max(Collection<E> c) {
-//        return c.stream().max(Comparator.naturalOrder());
-//    }
 
     public static void main(String[] args) {
         List<String> words = Arrays.asList("100", "20", "5");
 
         System.out.println(max(words));
 
-        // Using an optional to provide a chosen default value (Page 251)
-        //String lastWordInLexicon = max(words).orElse("No words...");
-        //System.out.println(lastWordInLexicon);
+
     }
 }
