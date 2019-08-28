67e1adc6421a69958def65ceb9708095dcdffb82
AltugAltintas <altuga@gmail.com>
2019-08-28 08:17:46 +0300

    question 38 added

---
+++ src/main/java/com/kodcu/question38/Main.java
@@ -0,0 +1,10 @@
+package com.kodcu.question38;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("start");
+        CountDownForMe countDownForMe = new CountDownForMe(100) ;
+        System.out.println("done");
+    }
+}
