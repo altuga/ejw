c5d5fbb06091c7da4bc9246657d5f145ab481ffc
AltugAltintas <altuga@gmail.com>
2019-09-03 10:30:20 +0300

    question02  is refactored

--- src/main/java/com/kodcu/question02/Roman.java
+++ src/main/java/com/kodcu/question02/Roman.java
@@ -11,18 +11,19 @@ package com.kodcu.question02;
  */
 
 /**
- * 1-  Measure the performance of the main() method
+ * 1-  Measure the performance of the main() method in MICROSECONDS
  * 2 - Can you make isRomanNumeral() method faster ?
  */
 public class Roman {
+
+
+    private static final String ALPHA_NUMERIC_STRING = "!?&ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
     static boolean isRomanNumeral(String s) {
         return s.matches("^(?=.)M*(C[MD]|D?C{0,3})"
                 + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
     }
 
-
-    private static final String ALPHA_NUMERIC_STRING = "!?&ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
     public static String randomAlphaNumeric(int count) {
         StringBuilder builder = new StringBuilder();
--- src/main/java/com/kodcu/question02/Sum.java
+++ src/main/java/com/kodcu/question02/Sum.java
@@ -1,8 +1,6 @@
 package com.kodcu.question02;
 
 
-import java.util.concurrent.TimeUnit;
-
 /**
  * Altug Bilgin Altintas
  * 
@@ -14,7 +12,7 @@ import java.util.concurrent.TimeUnit;
 
 /*
 TODO 
- 1 - Run the application
+ 1 - Measure the performance of the main() method in MILLISECONDS
  2 - Discussion : Did you feel any abnormality about the performance ?
  3 - Instructor will show the solutions
  4 - Refactor
