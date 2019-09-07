f6e3c39169af400ed7b812f816328ddb391868a9
AltugAltintas <altuga@gmail.com>
2019-09-07 11:48:52 +0300

    question 11 is refactored

--- src/main/java/com/kodcu/question11/ReadAndWriteFile.java
+++ src/main/java/com/kodcu/question11/ReadAndWriteFile.java
@@ -19,25 +19,28 @@ TODO:
 */
 public class ReadAndWriteFile {
 
+    private static final int BUFFER_SIZE = 8 * 1024;
 
-    private static final int BUFFER_SIZE = 1000;
-
-    public static void main(String[] args) {
-
-    }
-
-
-    private static void copy(String src, String dst) throws IOException {
-        
-        try (InputStream in = new FileInputStream(src);
-                 OutputStream out = new FileOutputStream(dst);){
-           
-         
+    static void copy(String src, String dst) throws IOException {
+        InputStream in = new FileInputStream(src);
+        try {
+            OutputStream out = new FileOutputStream(dst);
+            try {
                 byte[] buf = new byte[BUFFER_SIZE];
                 int n;
                 while ((n = in.read(buf)) >= 0)
                     out.write(buf, 0, n);
-           
-        } 
+            } finally {
+                out.close();
+            }
+        } finally {
+            in.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String src = args[0];
+        String dst = args[1];
+        copy(src, dst);
     }
 }
--- src/main/java/com/kodcu/question11/ReadFile.java
+++ src/main/java/com/kodcu/question11/ReadFile.java
@@ -21,28 +21,17 @@ TODO:
 */
 public class ReadFile {
 
-    private static final int BUFFER_SIZE = 8 * 1024;
-
-    static void copy(String src, String dst) throws IOException {
-        InputStream in = new FileInputStream(src);
+    static String firstLineOfFile(String path) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(path));
         try {
-            OutputStream out = new FileOutputStream(dst);
-            try {
-                byte[] buf = new byte[BUFFER_SIZE];
-                int n;
-                while ((n = in.read(buf)) >= 0)
-                    out.write(buf, 0, n);
-            } finally {
-                out.close();
-            }
+            return br.readLine();
         } finally {
-            in.close();
+            br.close();
         }
     }
 
     public static void main(String[] args) throws IOException {
-        String src = args[0];
-        String dst = args[1];
-        copy(src, dst);
+        String path = args[0];
+        System.out.println(firstLineOfFile(path));
     }
 }
