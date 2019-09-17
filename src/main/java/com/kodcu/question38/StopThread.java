aaac84fbf1966dafb22fad59b9e76dbaf075c4eb
AltugAltintas <altuga@gmail.com>
2019-09-17 16:20:30 +0300

    question 38 placeholder is done

--- src/main/java/com/kodcu/question38/StopThread.java
+++ src/main/java/com/kodcu/question38/StopThread.java
@@ -6,20 +6,22 @@ import java.util.concurrent.TimeUnit;
 * How long would you expect this program to run?
 */
 public class StopThread {
-    private static boolean stopRequested;
+    private static boolean stopRequested;  // 4
+
+    // 1
 
     public static void main(String[] args)
             throws InterruptedException {
         Thread backgroundThread = new Thread(() -> {
             int i = 0;
-            while (!stopRequested)
+            while (!stopRequested)  //2
                 i++;
             System.out.println("Finito");
         });
         backgroundThread.start();
 
         TimeUnit.SECONDS.sleep(1);
-        stopRequested = true;
+        stopRequested = true;  // 3
 
     }
 }
