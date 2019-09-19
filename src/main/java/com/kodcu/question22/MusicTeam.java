e04db14cd82674af8eef23937e08a75582dc71fc
AltugAltintas <altuga@gmail.com>
2019-09-19 21:30:43 +0300

    question 22

--- src/main/java/com/kodcu/question22/MusicTeam.java
+++ src/main/java/com/kodcu/question22/MusicTeam.java
@@ -2,7 +2,7 @@ package com.kodcu.question22;
 
 //1
 public enum MusicTeam {
-    GUITARMAN, SOLO, DUET, TRIO, QUARTET, QUINTET,
+    SOLO, DUET, TRIO, QUARTET, QUINTET,
     SEXTET, SEPTET, OCTET, NONET, DECTET;
     public int numberOfMusicians() { return ordinal() + 1; }
 }
